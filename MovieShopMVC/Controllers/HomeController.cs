6cb870db68f2b6e0a113147f2a1ee87d48a93123
qq472479340 <Zixun-Wang@outlook.com>
2021-08-09 18:44:56 -0500

    add

---
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Services;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieService _movieService;
+        public HomeController()
+        {
+            _movieService = new MovieService();
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _movieService.GetTopRevenueMovies();
+            ViewBag.PageTitle = "Top Revenue Movie";
+            ViewData["TotalMovies"] = movies.Count();
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
