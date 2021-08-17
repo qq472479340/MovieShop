7f5b5beca4e857801d7ff0990253241a2f6fa5e9
qq472479340 <Zixun-Wang@outlook.com>
2021-08-16 22:56:58 -0500

    add movie details

--- MovieShopMVC/Startup.cs
+++ MovieShopMVC/Startup.cs
@@ -34,6 +34,8 @@ namespace MovieShopMVC
             services.AddScoped<IMovieRepository, MovieRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICastRepository, CastRepository>();
+            services.AddScoped<ICastService, CastService>();
 
             services.AddDbContext<MovieShopDbContext>
                 (
